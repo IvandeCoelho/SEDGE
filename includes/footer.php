1312c5108f52eac4e72f4593ee1df0a49ff45c6a
IvandeCoelho <ivandealveslp@gmail.com>
2021-04-18 23:19:27 -0300

    add index

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<footer class="d-flex flex-column align-items-center text-small mt-5">
+
+
+    <div class="p-2 mb-2">
+        &copy; 2021 Ivande Coelho
+    </div>
+
+    <nav class="nav">
+        <ul class="list-inline text-center px-5">
+            <li class="list-inline-item p-2">
+                <a href="">Privacidade</a>
+            </li>
+            <li class="list-inline-item p-2">
+                <a href="">Termos</a>
+            </li>
+            <li class="list-inline-item p-2">
+                <a href="">Apoio, suporte</a>
+            </li>
+        </ul>
+    </nav>
+
+</footer>
---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+
+    <title>SEDGE</title>
+</head>
+
+<body>
+
+    <?php /*
+    unset(
+        $_SESSION['nome_user'],
+        $_SESSION['id_user'],
+        $_SESSION['email_user'],
+        $_SESSION['senha_user'],
+        $_SESSION['tipo_user']
+    );*/
+    ?>
+
+    <div class="container">
+        <form action="logar.php" method="post">
+            <h1 class="h1 text-center">LOGIN</h1>
+            <div class="row">
+                <div class="col-12">
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+                            required>
+                        <label for="floatingInput">Email</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                            required>
+                        <label for="floatingPassword">Senha</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-12">
+                    <input type="submit" value="LOGIN" class="btn btn-success btn-lg col-12">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6 text-center">
+                    Esqueceu sua senha? <a href="recuperar_senha.php">Clique aqui</a>
+                </div>
+                <div class="col-lg-6 text-center">
+                    Não tenho cadastro? <a href="cadastro_de_usuarios.php">Clique aqui</a>
+                </div>
+            </div>
+        </form>
+
+        <?php
+        include_once 'includes/footer.php';
+        ?>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
